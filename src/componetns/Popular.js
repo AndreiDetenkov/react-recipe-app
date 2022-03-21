9bcd58b50c24798b60147503f89301b5f674e1cb
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-03-21 15:52:15 +0600

    refactor: cache data in localstorage

--- src/componetns/Popular.js
+++ src/componetns/Popular.js
@@ -1,4 +1,9 @@
 import React, {useState, useEffect} from 'react';
+import styled from 'styled-components'
+import {Splide, SplideSlide} from '@splidejs/react-splide'
+
+import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
+
 
 const Popular = () => {
 
@@ -13,6 +18,12 @@ const Popular = () => {
   const getPopular = async () => {
     const number = 10
     const apiKey = process.env.REACT_APP_API_KEY
+    const check = localStorage.getItem('popular')
+
+    if (check) {
+      setPopular(JSON.parse(check))
+      return
+    }
 
     try {
       setIsLoading(true)
@@ -21,6 +32,8 @@ const Popular = () => {
           return response.json();
         })
         .then((data) => {
+          console.log(data.recipes)
+          localStorage.setItem('popular', JSON.stringify(data.recipes))
           setPopular(data.recipes)
         });
     } catch (error) {
@@ -31,13 +44,74 @@ const Popular = () => {
     }
   }
 
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div>
-      {popular.map(recipe => {
-        return <p key={recipe.id.toString()}>{recipe.title}</p>
-      })}
-    </div>
+    <Wrapper>
+      <h3>Popular picks</h3>
+
+      <Splide options={{perPage: 4, arrows: false, pagination: false, drag: 'free', gap: '5rem'}}>
+        {popular.map(recipe => {
+          return (
+            <SplideSlide key={recipe.id.toString()}>
+              <Card>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title}/>
+                <Gradient/>
+              </Card>
+            </SplideSlide>
+          )
+        })}
+      </Splide>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  margin: 4rem 0;
+`
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1.5rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`
 
 export default Popular;
