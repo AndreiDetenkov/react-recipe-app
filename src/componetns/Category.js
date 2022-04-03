52d0ae9888adac99819811b24ba75c1caf4d14d4
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-04-03 15:08:04 +0600

    style: style category menu

--- src/componetns/Category.js
+++ src/componetns/Category.js
@@ -7,25 +7,25 @@ import styled from 'styled-components'
 const Category = () => {
   return (
     <List>
-      <NavLink to={'/cuisine/Italian'}>
+      <StyledLink to={'/cuisine/Italian'}>
         <FaPizzaSlice/>
         <h4>Italian</h4>
-      </NavLink>
+      </StyledLink>
 
-      <NavLink to={'/cuisine/American'}>
+      <StyledLink to={'/cuisine/American'}>
         <FaHamburger/>
         <h4>American</h4>
-      </NavLink>
+      </StyledLink>
 
-      <NavLink to={'/cuisine/Thai'}>
+      <StyledLink to={'/cuisine/Thai'}>
         <GiNoodles/>
         <h4>Thai</h4>
-      </NavLink>
+      </StyledLink>
 
-      <NavLink to={'/cuisine/Japanese'}>
+      <StyledLink to={'/cuisine/Japanese'}>
         <GiChopsticks/>
         <h4>Japanese</h4>
-      </NavLink>
+      </StyledLink>
     </List>
   );
 };
@@ -34,6 +34,44 @@ const List = styled.div`
   display: flex;
   justify-content: center;
   margin: 2rem 0;
+`
+
+const StyledLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 20px;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  height: 6rem;
+  width: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+  
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+  }
+  
+  svg {
+    color: white;
+    font-size: 1.5rem;
+    margin-bottom: 0.2rem;
+  }
+  
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+    
+    svg {
+      color: white;
+    }
+    
+    h4 {
+      color: white
+    }
+  }
 `
 
 export default Category;
